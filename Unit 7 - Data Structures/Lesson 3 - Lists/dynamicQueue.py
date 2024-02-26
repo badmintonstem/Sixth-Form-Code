681f7f4a1c72cb296903b50918ba52b616adfa8e
badmintonstem <jhope@badmintonschool.co.uk>
2024-02-26 14:44:16 +0000

    added a queue coded using a dynamic list

--- Unit 7 - Data Structures/Lesson 3 - Lists/dynamicQueue.py
+++ Unit 7 - Data Structures/Lesson 3 - Lists/dynamicQueue.py
@@ -1,13 +1,37 @@
 class DynamicQueue:
-    def __init__(self):
+    def __init__(self, maxSize=None):
         self.items = []
+        self.maxSize = maxSize
     
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
+
+    def isFull(self):
+        return len(self.items) == self.maxSize
 
     def enqueue(self, newItem):
-        pass
+        if self.isFull():
+            print("Queue is full")
+        else:
+            self.items.append(newItem)
             
     def dequeue(self):
-        pass
+        if self.isEmpty():
+            print("Queue is empty")
+        else:
+            return self.items.pop(0)
    
+myToDoList = DynamicQueue(3)
+print(myToDoList.isEmpty())
+print(myToDoList.isFull())
+myToDoList.enqueue("Dishes")
+myToDoList.enqueue("Cooking")
+print(myToDoList.isEmpty())
+print(myToDoList.isFull())
+myToDoList.enqueue("Shoping")
+myToDoList.enqueue("Watch TV")
+print(myToDoList.dequeue())
+print(myToDoList.dequeue())
+print(myToDoList.dequeue())
+print(myToDoList.dequeue())
+print(myToDoList.dequeue())
