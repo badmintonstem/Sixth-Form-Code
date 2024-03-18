367e94c7a9b31da08dd26db7246cedb405d6fb2c
badmintonstem <jhope@badmintonschool.co.uk>
2024-03-18 13:35:34 +0000

    Updated binary search function

--- Unit 12 - Algorithms/searching algorithms.py
+++ Unit 12 - Algorithms/searching algorithms.py
@@ -51,6 +51,7 @@ if __name__ == '__main__':
     except OSError as e:
         print("Unable to read word file {}".format(e))
     
+    total_list_length = len(words)
     girlsNames = ['Amelia', 'Ava', 'Emily', 'Isabella', 'Isla', 'Jessica', 'Lily', 'Olivia', 'Poppy', 'Sophie']
     #print(words)
     #numberList = [randint(0,100) for _ in range(50)]
@@ -59,10 +60,31 @@ if __name__ == '__main__':
     word = 'zulu'
     item = linearSearch(words, word)
     time = timeit.timeit('linearSearch(words, word)', setup = 'from __main__ import linearSearch, words, word', number = REPEATS)
-    print(f'it took {time}s to find the word {word} with a linear search')
+    print(f'\nIt took {time:.4f}s to find the word {word} with a linear search')
     item = binarySearchI(words, word)
     time = timeit.timeit('binarySearchR(words, word)', setup = 'from __main__ import binarySearchR, words, word', number = REPEATS)
-    print(f'it took {time}s to find the word {word} with a binary search (recursive)')
+    print(f'\nIt took {time:.4f}s to find the word {word} with a binary search (recursive)')
     item = binarySearchR(words, word)
     time = timeit.timeit('binarySearchI(words, word)', setup = 'from __main__ import binarySearchI, words, word', number = REPEATS)
-    print(f'it took {time}s to find the word {word} with a binary search (iterative)')
+    print(f'\nIt took {time:.4f}s to find the word {word} with a binary search (iterative)')
+    print('\n\n')
+    print("would you like to graph the time complexity?\nPress enter to continue.")
+    input()
+    results = []
+    for x in range(101,0,-1):
+        length = total_list_length//x
+        short_list=words[-length:]
+        linear_time = timeit.timeit('linearSearch(short_list, word)', setup = 'from __main__ import linearSearch, short_list, length, word', number = REPEATS)
+        binary_time = timeit.timeit('binarySearchI(short_list, word)', setup = 'from __main__ import binarySearchI, short_list, length, word', number = REPEATS)
+        binary_time_R = timeit.timeit('binarySearchR(short_list, word)', setup = 'from __main__ import binarySearchR, short_list, length, word', number = REPEATS)
+        results.append([length,linear_time,binary_time,binary_time_R])
+    
+    print('|  List Length  |  Linear Time  |  Binary Time  | Binary Time-R |')
+    with open('results.csv','w') as f:
+        f.write('')
+    with open('results.csv','a') as f:
+        f.write('List Length, Linear Time, Binary Time, Binary Time-R\n')
+        for row in results:
+            print(f'|{row[0]: ^15}|{row[1]:^15.5f}|{row[2]:^15.5f}|{row[3]:^15.5f}|')
+            f.write(f'{row[0]}, {row[1]}, {row[2]}, {row[3]}\n')
+    #print(results)
